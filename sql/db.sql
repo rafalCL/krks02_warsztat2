c70bfa31bb363b43b7a95481b8fb2cff730bd1e5
rafalcl <rafalclab@gmail.com>
2018-02-16 15:18:52 +0100

    Add users table schema

--- sql/db.sql
+++ sql/db.sql
@@ -1,3 +1,13 @@
 CREATE DATABASE krks02_warsztat2
-DEFAULT CHARACTER SET utf8
-DEFAULT COLLATE utf8_general_ci;
+	DEFAULT CHARACTER SET utf8
+	DEFAULT COLLATE utf8_general_ci;
+
+-- Table users schema
+CREATE TABLE users(
+	id INT AUTO_INCREMENT,
+	username VARCHAR(255) UNIQUE,
+	email VARCHAR(255) UNIQUE,
+	password VARCHAR(255),
+	person_group_id INT,
+	PRIMARY KEY(id)
+);
